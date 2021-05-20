e07d385aa5a7958525e5c2ff15bbf965b5595f8a
Vahin Sharma <19247154+vs-123@users.noreply.github.com>
2021-05-20 18:15:04 +0530

    Update index.go

--- index.go
+++ index.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"gopkg.in/olahol/melody.v1"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	m := melody.New()
+	// port := os.Getenv("PORT")
 
 	r.GET("/", func(c *gin.Context) {
 		http.ServeFile(c.Writer, c.Request, "index.html")
@@ -21,7 +24,11 @@ func main() {
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		if splitMsg := strings.Split(string(msg), " "); splitMsg[0] == "CLIENTCLOSING" {
+			m.Broadcast([]byte("<i>" + splitMsg[1] + " <red>left</red> the chat</i>"))
+		} else {
+			m.Broadcast(msg)
+		}
 	})
 
 	r.Run(":" + os.Getenv("PORT"))
